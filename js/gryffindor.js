b6007e4aeea1428107fc7ab33a1a1f727911280f
Nariah-rgb <527nhenderson@frhsd.com>
2026-05-21 21:54:09 +0000

    styling clean up

--- js/gryffindor.js
+++ js/gryffindor.js
@@ -100,9 +100,6 @@ const showStudentModal = (selectedStudent) => {
                         </div>
                         <div class="modal-body" id="studentModalBody">
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
                     </div>
                 </div>
             `;
